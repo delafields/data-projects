030f221b771095a68caba07ee4d0287404d682d6
delafields <j.c.delafields@gmail.com>
2020-01-03 20:52:52 -0500

    Spend vs. Rank data munged - just need to get the animation working then styling

--- epl-transfers/TODO.md
+++ epl-transfers/TODO.md
@@ -1,10 +1,6 @@
 # TODOs
 
 1. Make the call on whether to use RMD or a juypter notebook
-2. Do I want to adjust for inflation? Thinking yes. Consider how to do this properly.
-3. Create functions to..
-    * Aggregate net sales by team
-    * Grab number of transfers
 
 ## Overarching question and the primary thing being explored
 * What teams got an unexpected injection of $? When did this happen? What was the result?
@@ -12,17 +8,11 @@
 ## Additional data points // some anecdotal things
 * Who conducts the most loans?
     * This could be as simple as a well designed countplot.
-* Who gets the most bang for their buck (spends the least and gets results)
-    * Maybe plot little $'s onto how many points they got?
-* How many transfers per win/point gained // how much was spent per win/point gained?
-    * Can I reliably attribute points gained just to transfers for one year?
 
 ## Pure visualization
 * Net transfer $ ranking vs. end of season ranking
-    * Rankings are already baked into the scraped data, just need to join this to a function that calcs transfer $ rank
-    * I'm thinking this can be a dot pair plot
-        * I would love for this to be interactive, so the user can select the year and it can populate accordingly
-        * Maybe do an "overall" one and average ranking? (Gonna have to rank 0 for relegated teams)
+    * Data is in the right place. Just need to GET THE ANIMATION DOWN
+    * Maybe do an "overall" one and average ranking? (Gonna have to rank 0 for relegated teams)
 * How have transfer amounts changed per position year over year?
-    * I'm thinking this can be a joy-plot in R.
-    * Pretty simple to put together, just needs inflation adjustments.
+    * This is going to be a line plot - the only thing left to explore is whether to group by D/MF/Fwds
+    * DONE except for styling
---
+++ epl-transfers/spendVSrank.R
@@ -0,0 +1,190 @@
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(ggalt)
+library(plotly)
+
+# merges multiple csvs together
+multmerge = function(path){
+    filenames = list.files(path=path, full.names=TRUE)
+    
+    datalist = lapply(filenames, function(x){
+        read.csv(file = x, header = T)})
+    
+    Reduce(function(x, y) {merge(x, y, all = TRUE)}, datalist)
+}
+
+# merge the transfer data
+transfer_data <- multmerge("data/transfer-data")
+# merge results data
+results_data <- multmerge("data/epl-results")
+
+# only keep these columns
+cols <- c("club_name", "fee_cleaned", "year", "transfer_movement", "season")
+transfer_data <- transfer_data[cols]
+
+# replace na's with 0's
+transfer_data[is.na(transfer_data)] <- 0
+
+# multiply "out" transfers by -1
+transfer_data <- transfer_data %>%
+    mutate(correct_fee = ifelse(transfer_movement == 'out', fee_cleaned * -1, fee_cleaned))
+
+# rank transfer amount ranking for each year
+grouped_transfer_data <- transfer_data %>%
+    group_by(club_name, year) %>%
+    summarise(total_spend = sum(correct_fee))
+
+grouped_transfer_data <- grouped_transfer_data %>%
+    arrange(year) %>%
+    mutate(spend_rank = dense_rank(desc(total_spend)))
+
+# rename `club_name` to `Team` for later join
+grouped_transfer_data <- rename(grouped_transfer_data, Team = club_name)
+
+#temp <- grouped_transfer_data %>% filter(year == 2018)
+
+# some regex work before joining
+# trim year in the results df
+# remove the (C) for Champion and (R) for relegated from Team name
+results_data <- results_data %>% 
+    mutate(season = year) %>%
+    mutate(year = substr(year, start = 1, stop = 4)) %>%
+    mutate(Team = str_replace_all(Team, " \\(\\S\\)", ""))
+
+
+# rename transfer data team names to match results data
+# remove FC and AFC from names then strip strings
+grouped_transfer_data <- grouped_transfer_data %>%
+    mutate(Team = str_replace_all(Team, " FC", "")) %>%
+    mutate(Team = str_replace_all(Team, "AFC", "")) %>%
+    mutate(Team = trimws(Team))
+
+# convert results_data `year` to numeric for later joining
+results_data <- results_data %>%
+    mutate(year = as.numeric(year))
+
+# join transfers to results
+data <- results_data %>% left_join(grouped_transfer_data, by = c("Team", "year"))
+
+
+
+
+###
+# PLOTTING
+###
+temp <- data %>% filter(year == 2001)
+
+
+p <- ggplot(data, aes(x=Pos, xend=spend_rank, y=Team)) + 
+    geom_segment(aes(x=Pos, 
+                     xend=spend_rank, 
+                     y=Team, 
+                     yend=Team), 
+                 color="#b2b2b2", size=1.5)+
+    geom_dumbbell(color="red", 
+                  size_x=3.5, 
+                  size_xend = 3.5,
+                  #Note: there is no US:'color' for UK:'colour' 
+                  # in geom_dumbbel unlike standard geoms in ggplot()
+                  colour_x="#edae52", 
+                  colour_xend = "#9fb059")+
+    labs(x=NULL, y=NULL, 
+         title="Dumbbell Chart", 
+         subtitle="Spend vs. End of Season Rank")+
+    geom_text(color="black", size=2, hjust=-0.5,
+              aes(x=Pos, label=Pos))+
+    geom_text(aes(x=spend_rank, label=spend_rank), 
+              color="black", size=2, hjust=1.5)
+p
+
+
+p <- plot_ly(data, color = I("gray80")) %>%
+    add_segments(x = ~Pos, xend = ~spend_rank, y = ~Team, yend = ~Team, showlegend = FALSE) %>%
+    add_markers(x = ~Pos, y = ~Team, name = "Pos", color = I("pink")) %>%
+    add_markers(x = ~spend_rank, y = ~Team, name = "spend_rank", color = I("blue")) %>%
+    layout(
+        title = "Gender earnings disparity",
+        xaxis = list(title = "Annual Salary (in thousands)"),
+        margin = list(l = 65)
+    ) 
+
+p
+
+
+## temp
+gg <- ggplot(data, aes(x=Pos, xend=spend_rank, y=Team, frame=year)) + 
+    geom_segment(aes(x=Pos, 
+                     xend=spend_rank, 
+                     y=Team, 
+                     yend=Team), 
+                 color="#b2b2b2", size=1.5)+
+    geom_dumbbell(color="red", 
+                  size_x=3.5, 
+                  size_xend = 3.5,
+                  #Note: there is no US:'color' for UK:'colour' 
+                  # in geom_dumbbel unlike standard geoms in ggplot()
+                  colour_x="#edae52", 
+                  colour_xend = "#9fb059")+
+    labs(x=NULL, y=NULL, 
+         title="Dumbbell Chart", 
+         subtitle="Spend vs. End of Season Rank")+
+    geom_text(color="black", size=2, hjust=-0.5,
+              aes(x=Pos, label=Pos))+
+    geom_text(aes(x=spend_rank, label=spend_rank), 
+              color="black", size=2, hjust=1.5)
+
+options(warn=1)
+ggplotly(gg)
+
+## temp not wokring
+library("crosstalk")
+
+newdata <- SharedData$new(data)
+
+widgets <- bscols(
+    #filter_select("year", "Year", newdata, ~year)
+    #filter_slider("sales", "Sales", newdata, ~sales),
+    filter_checkbox("year", "Years", newdata, ~year, inline = TRUE)
+)
+widgets
+
+bscols(
+    widths = c(4, 8), widgets, 
+    plot_ly(data, color = I("gray80")) %>%
+        add_segments(x = ~Pos, xend = ~spend_rank, y = ~Team, yend = ~Team, showlegend = FALSE) %>%
+        add_markers(x = ~Pos, y = ~Team, name = "Pos", color = I("pink")) %>%
+        add_markers(x = ~spend_rank, y = ~Team, name = "spend_rank", color = I("blue")) %>%
+        layout(
+            title = "Gender earnings disparity",
+            xaxis = list(title = "Annual Salary (in thousands)"),
+            margin = list(l = 65)
+        ) 
+)
+
+## temp
+
+tx <- highlight_key(data, ~year, "Select a city")
+
+gg <- p
+select <- highlight(
+    ggplotly(gg, tooltip = "year"), 
+    selectize = TRUE, persistent = TRUE
+)
+
+bscols(select)
+
+## temp using ggplot dumbell
+# see link below for moving labels and changing them
+# https://stackoverflow.com/questions/58507077/gganimate-change-axes-between-frames
+p + transition_time(year) +
+    labs(title = "Year: {frame_time}")
+
+animation <- p +  
+    labs(title = 'Year: {closest_state}', x = 'Top 5', y = 'Bottom 5') +
+    transition_states(year) +
+    ease_aes('linear')
+
+animate(animation, renderer = gifski_renderer("gganim.gif"))
+
+anim_save("filenamehere.gif", anim)
