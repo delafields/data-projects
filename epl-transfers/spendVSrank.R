95eca963100e27ba3b27828a59b827e326b15663
delafields <j.c.delafields@gmail.com>
2020-01-07 21:46:16 -0500

    Spend vs Rank working.

--- epl-transfers/spendVSrank.R
+++ epl-transfers/spendVSrank.R
@@ -2,6 +2,186 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 library(ggalt)
+library(showtext)
+
+# merges multiple csvs together
+multmerge = function(path){
+    filenames = list.files(path=path, full.names=TRUE)
+    
+    datalist = lapply(filenames, function(x){
+        read.csv(file = x, header = T)})
+    
+    Reduce(function(x, y) {merge(x, y, all = TRUE)}, datalist)
+}
+
+# merge the transfer data
+transfer_data <- multmerge("data/transfer-data")
+# merge results data
+results_data <- multmerge("data/epl-results")
+
+# only keep these columns
+cols <- c("club_name", "fee_cleaned", "year", "transfer_movement", "season")
+transfer_data <- transfer_data[cols]
+
+# replace na's with 0's
+transfer_data[is.na(transfer_data)] <- 0
+
+# multiply "out" transfers by -1
+transfer_data <- transfer_data %>%
+    mutate(correct_fee = ifelse(transfer_movement == 'out', fee_cleaned * -1, fee_cleaned))
+
+# rank transfer amount ranking for ALL years
+grouped_transfer_data <- transfer_data %>%
+    group_by(club_name) %>%
+    summarise(total_spend = sum(correct_fee))
+
+grouped_transfer_data <- grouped_transfer_data %>%
+    arrange(total_spend) %>%
+    mutate(spend_rank = dense_rank(desc(total_spend)))
+
+# rename `club_name` to `Team` for later join
+grouped_transfer_data <- rename(grouped_transfer_data, Team = club_name)
+
+# some regex work before joining
+# trim year in the results df
+# remove the (C) for Champion and (R) for relegated from Team name
+results_data <- results_data %>% 
+    mutate(season = year) %>%
+    mutate(year = substr(year, start = 1, stop = 4)) %>%
+    mutate(Team = str_replace_all(Team, " \\(\\S\\)", ""))
+
+
+# rename transfer data team names to match results data
+# remove FC and AFC from names then strip strings
+grouped_transfer_data <- grouped_transfer_data %>%
+    ungroup(Team) %>%
+    mutate(Team = str_replace_all(Team, " FC", "")) %>%
+    mutate(Team = str_replace_all(Team, "AFC", "")) %>%
+    mutate(Team = trimws(Team))
+
+# convert results_data `year` to numeric for later joining
+results_data <- results_data %>%
+    mutate(year = as.numeric(year))
+
+# get average season-end position for each team
+grouped_results_data <- results_data %>%
+    group_by(Team) %>%
+    summarise(avg_Pos = mean(Pos, na.rm = TRUE)) %>% 
+    mutate(avg_Pos = round(avg_Pos, digits = 0))
+
+# filter transfer data to top 10
+top_10_spenders <- grouped_transfer_data %>%
+    filter(spend_rank < 11)
+
+# join transfers to results
+data <- grouped_results_data %>% 
+    right_join(top_10_spenders, by = c("Team")) %>%
+    arrange(spend_rank)
+
+#data$spend_rank <- factor(data$spend_rank, levels=as.numeric(data$spend_rank))
+
+
+############
+# PLOTTING #
+############
+
+## Loading Google fonts (http://www.google.com/fonts)
+font_add_google("Poppins", "poppins")
+
+## Automatically use showtext to render text for future devices
+showtext_auto()
+
+## Plotting functions as usual
+## Open a graphics device if you want, e.g.
+## png("demo.png", 700, 600, res = 96)
+## If you want to show the graph in a window device,
+## remember to manually open one in RStudio
+## See the "Known Issues" section
+windows()
+
+ggplot(data, aes(x=avg_Pos, xend=spend_rank, y=Team)) + 
+    geom_segment(aes(x=avg_Pos, 
+                     xend=spend_rank, 
+                     y=Team, 
+                     yend=Team), 
+                 color="black", size=1)+
+    geom_dumbbell(color=NA,
+                  size_x=6, 
+                  size_xend = 6,
+                  #Note: there is no US:'color' for UK:'colour' 
+                  # in geom_dumbbel unlike standard geoms in ggplot()
+                  colour_x="#38003c", 
+                  colour_xend = "#00ff85")+
+    labs(x="Position", y=NULL, 
+         title="Buying wins?", 
+         subtitle="Transfer Budget vs. Average Table Position (1992-2018)")+
+    geom_text(color="white", size=3, #hjust=-0.5,
+              aes(x=avg_Pos, label=avg_Pos, fontface="bold"))+
+    geom_text(aes(x=spend_rank, label=spend_rank, fontface="bold"), 
+              color="black", size=3) + #, hjust=1.5)
+    theme(plot.title = element_text(face = "bold", color = "#38003c"),
+          axis.title.x = element_text(face = "bold"),
+          panel.background = element_blank(),
+          panel.grid.major = element_line(colour = "#e0e0e0", linetype = "dashed", size=0.1),
+          axis.title.y = element_text(face = "bold"),
+          text = element_text(family = "poppins"))
+
+
+health <- data
+health$Team <- factor(health$Team, levels=as.character(health$Team))
+
+
+ggplot(health, aes(x=avg_Pos, xend=spend_rank, y=Team)) + 
+    #create a thick line between x and xend instead of using defaut 
+    #provided by geom_dubbell
+    geom_segment(aes(x=avg_Pos, 
+                     xend=spend_rank, 
+                     y=Team, 
+                     yend=Team), 
+                 color="black", size=1)+
+    geom_dumbbell(color=NA,
+                  size_x=6, 
+                  size_xend = 6,
+                  #Note: there is no US:'color' for UK:'colour' 
+                  # in geom_dumbbel unlike standard geoms in ggplot()
+                  colour_x="#38003c", 
+                  colour_xend = "#00ff85")+
+    labs(x="\nPosition", y=NULL, 
+         title="Buying wins?", 
+         subtitle="Transfer Budget vs. Average Table Position (1992-2018)")+
+    geom_text(color="white", size=3, #hjust=-0.5,
+              aes(x=avg_Pos, label=avg_Pos, fontface="bold"))+
+    geom_text(aes(x=spend_rank, label=spend_rank, fontface="bold"), 
+              color="black", size=3) + 
+    theme(plot.title = element_text(face = "bold", color = "#38003c"),
+          axis.title.x = element_text(face = "bold"),
+          panel.background = element_blank(),
+          panel.grid.major = element_line(colour = "#e0e0e0", linetype = "dashed", size=0.1),
+          axis.title.y = element_text(face = "bold"),
+          text = element_text(family = "poppins")) +
+        geom_rect(aes(xmin = 12.25, xmax = 13.75, ymin = 2.5, ymax = 3),
+              fill = "#00ff85", color = "#00ff85", size = 1.5) + 
+    annotate(geom="text", x=13, y=2.8, label="Spend Rank") + 
+    geom_rect(aes(xmin = 12.25, xmax = 13.75, ymin = 2, ymax = 2.5),
+              fill = "#38003c", color = "#38003c", size = 1.5) + 
+    annotate(geom="text", x=13, y=2.25, label="Avg Pos", color="white")
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(ggalt)
 library(plotly)
 library(gganimate)
 
