f6ec4b8a48eda098410bda579762ed10988e4892
delafields <j.c.delafields@gmail.com>
2020-01-07 19:23:16 -0500

    Positional plot done. Just need to pick which to save to file.

--- epl-transfers/positionsYoY.R
+++ epl-transfers/positionsYoY.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(showtext)
 library(ggplot2)
 library(cowplot)
 library(plotly)
@@ -62,12 +63,26 @@ grouped_data <- grouped_data %>%
 ###
 # PLOTTING
 ###
+## Loading Google fonts (http://www.google.com/fonts)
+font_add_google("Poppins", "poppins")
+
+## Automatically use showtext to render text for future devices
+showtext_auto()
+
+## Plotting functions as usual
+## Open a graphics device if you want, e.g.
+## png("demo.png", 700, 600, res = 96)
+## If you want to show the graph in a window device,
+## remember to manually open one in RStudio
+## See the "Known Issues" section
+windows()
+
 grouped_data <- rename(grouped_data, Position = position) #For renaming dataframe column
 
 
 # clean up the styling
 ggplot(grouped_data, aes(x = year, y = total_spend)) +
-    geom_line(aes(color = position, linetype = position)) +
+    geom_line(aes(color = Position, linetype = Position)) +
     facet_wrap(~ pos_group, nrow = 3)
 
 lineplotter <- function(df_group) {
@@ -76,54 +91,57 @@ lineplotter <- function(df_group) {
         scale_x_continuous(breaks = pretty(df_group$year, n = 10)) +
         labs(x = "\nYear") + 
         theme(plot.title = element_text(face = "bold", color = "#38003c", margin = margin(10, 0, 10, 0)),
-              axis.title.x = element_text(face = "bold", color = "#38003c"),
-              axis.title.y = element_text(face = "bold", color = "#38003c"),
-              axis.text.x = element_text(color = "#38003c"),
-              axis.text.y = element_text(color = "#38003c"),
-              text = element_text(family = "URWGothic"),
+              axis.title.x = element_text(face = "bold"),
+              axis.title.y = element_text(face = "bold"),
+              text = element_text(family = "poppins"),
               panel.grid.major = element_line(colour = "#e0e0e0", linetype = "dashed", size=0.1),
+              panel.grid.major.x = element_blank(),
               panel.grid.minor = element_blank(),
               panel.border = element_blank(), 
               #axis.line = element_line(),
               axis.line.x = element_line(),
               panel.background = element_blank(),
-              legend.title = element_text(face = "bold", color = "#38003c"),
-              legend.text = element_text(color = "#38003c"),
-              #legend.background = element_rect(linetype="solid", color = "#38003c", size = 1),
+              legend.title = element_text(face = "bold"),
               legend.key=element_rect(fill='white')) + 
         ylim(-125, 275) +
-        ggtitle("Spend per Position in the Prem (millions £)\n")
-        #+ scale_color_manual(values=c("#04f5ff", "#e90052", "#00ff85", "#38003c", "#ebfe05", "#a6004c"))
-    }
+        ggtitle("People in the Center are getting PAID",
+            subtitle = "Spend per position in the Prem (millions £)\n") +
+        scale_color_manual(values=c("#04f5ff", "#e90052", "#00ff85", "#ebfe05", "#38003c", "#500057")) + 
+        annotate(geom="text", x=1993, y=150, label=df_group$pos_group)
+}
 
 mf_plot <- lineplotter(grouped_data %>% filter(pos_group == 'Midfield'))
 fwd_plot <- lineplotter(grouped_data %>% filter(pos_group == 'Forward'))
 def_plot <- lineplotter(grouped_data %>% filter(pos_group == 'Defense'))
 
 
-plot_grid(fwd_plot + theme(axis.title.x = element_blank(),
-                           axis.title.y = element_blank()), 
-          mf_plot + theme(axis.title.x = element_blank(),
-                          axis.title.y = element_blank(),
-                          plot.title = element_blank()), 
-          def_plot + theme(axis.title.y = element_blank(),
-                           plot.title = element_blank()),  
+plot_grid(fwd_plot + 
+              theme(axis.title.x = element_blank(),
+                    axis.title.y = element_blank()), 
+          mf_plot + 
+              theme(axis.title.x = element_blank(),
+                    axis.title.y = element_blank(),
+                    plot.title = element_blank(),
+                    plot.subtitle = element_blank()), 
+          def_plot + 
+              theme(axis.title.y = element_blank(),
+                    plot.title = element_blank(),
+                    plot.subtitle = element_blank()),  
           ncol = 1, 
           align = "v")
 
 
-
 # plotly
 mf_pplot <- plot_ly(grouped_data %>% filter(pos_group == 'Midfield'), color = ~position,
-        x = ~year, y = ~total_spend) %>%
+                    x = ~year, y = ~total_spend) %>%
     add_lines()
 
 fwd_pplot <- plot_ly(grouped_data %>% filter(pos_group == 'Forward'), color = ~position,
-                    x = ~year, y = ~total_spend) %>%
+                     x = ~year, y = ~total_spend) %>%
     add_lines()
 
 def_pplot <- plot_ly(grouped_data %>% filter(pos_group == 'Defense'), color = ~position,
-                    x = ~year, y = ~total_spend) %>%
+                     x = ~year, y = ~total_spend) %>%
     add_lines()
 
 subplot(mf_pplot, fwd_pplot, def_pplot, nrows = 3)
