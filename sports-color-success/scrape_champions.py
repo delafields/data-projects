10d5d059326b3a5a6e529b83b9a1edceecc6353a
delafields <j.c.delafields@gmail.com>
2020-01-22 13:11:44 -0500

    All data correctly scraped and transformed.

--- sports-color-success/scrape_champions.py
+++ sports-color-success/scrape_champions.py
@@ -23,59 +23,83 @@ def get_champs_table(league):
 
         return champ_df
 
-# SERIE A DONE
-# champ_df = get_champs_table(league = "Serie A")[["Club", "Champions", "League"]]
-# champ_df = champ_df.rename(columns = {"Club": "Team", "Champions": "Wins"})
-# champ_df = champ_df.replace({"Milan": "A.C. Milan", "Internazionale": "Inter Milan", "Roma": "A.S. Roma"}) # don't have many colors
+for league in championship_urls:
+        if league == "Serie A":
+                champ_df = get_champs_table(league = league)[["Club", "Champions", "League"]]
+                champ_df = champ_df.rename(columns = {"Club": "Team", "Champions": "Wins"})
+                # don't have many here
+                champ_df = champ_df.replace({"Milan": "A.C. Milan", 
+                                             "Internazionale": "Inter Milan", 
+                                             "Roma": "A.S. Roma"})
+                                             
+                league = league.replace(" ", "")
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
 
-# LA LIGA DONE
-# champ_df = get_champs_table(league = "La Liga")[["Club", "Winners", "League"]]
-# champ_df = champ_df.rename(columns = {"Club": "Team", "Winners": "Wins"})
-# champ_df = champ_df.replace({"Real Madrid": "Real Madrid CF", "Barcelona": "Barcelona FC", "Atlético Madrid": "Atletico Madrid",
-#                              "Valencia": "Valencia CF", "Deportivo La Coruña": "Deportivo"})
+        elif league == "La Liga":
+                champ_df = get_champs_table(league = league)[["Club", "Winners", "League"]]
+                champ_df = champ_df.rename(columns = {"Club": "Team", "Winners": "Wins"})
+                champ_df = champ_df.replace({"Real Madrid": "Real Madrid CF", 
+                                             "Barcelona": "Barcelona FC", 
+                                             "Atlético Madrid": "Atletico Madrid",
+                                             "Valencia": "Valencia CF", 
+                                             "Deportivo La Coruña": "Deportivo"})
 
-# EPL DONE
-# champ_df = get_champs_table(league = "EPL")[["Club", "Winners", "League"]]
-# champ_df = champ_df.rename(columns = {"Club": "Team", "Winners": "Wins"})
-# champ_df = champ_df.replace({"Everton FC": "Everton", "Newcastle United": "Newcastle United FC", 
-#                              "Wolverhampton Wanderers": "Wolverhampton", "Watford": "Watford FC",
-#                              "Southampton": "Southampton FC"})
+                league = league.replace(" ", "")
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
 
-# NHL DONE
-# champ_df = get_champs_table(league = "NHL")[["Team", "Wins", "League"]]
-# champ_df["Team"] = champ_df["Team"].str.replace(r"\[.\]", "")
+        elif league == "EPL":
+                champ_df = get_champs_table(league = league)[["Club", "Winners", "League"]]
+                champ_df = champ_df.rename(columns = {"Club": "Team", "Winners": "Wins"})
+                champ_df = champ_df.replace({"Everton FC": "Everton", 
+                                             "Newcastle United": "Newcastle United FC", 
+                                             "Wolverhampton Wanderers": "Wolverhampton", 
+                                             "Watford": "Watford FC",
+                                             "Southampton": "Southampton FC"})
 
-# MLB DONE
-# champ_df = get_champs_table(league = "MLB")[["Team", "Wins", "League"]]
-# champ_df = champ_df.replace({"New York/San Francisco GiantsNY until 1957, SF 1958-present": "San Francisco Giants",
-#                              "Brooklyn/Los Angeles DodgersBKN until 1957, LA 1958-present": "Los Angeles Dodgers",
-#                              "Philadelphia/Kansas City/Oakland AthleticsPHI until 1954, KC 1955-1967, OAK 1968-present": "Oakland Athletics",
-#                              "Boston/Milwaukee/Atlanta BravesBOS until 1952, MIL 1953-1965, ATL 1966-present": "Atlanta Braves",
-#                              "St. Louis Browns/Baltimore OriolesSTL until 1953, BAL 1954-present": "Baltimore Orioles",
-#                              "Washington Senators/Minnesota TwinsWSH until 1960, MIN 1961-present": "Minnesota Twins",
-#                              "Washington Senators/Texas Rangers WSH until 1971, TEX 1972-present": "Texas Rangers",
-#                              "Montreal Expos/Washington Nationals MTL until 2004, WSH 2005-present": "Washington Nationals",
-#                              "Seattle Pilots/Milwaukee Brewers SEA 1969, MIL 1970-present": "Milwaukee Brewers"})
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
 
-# NBA DONE
-# champ_df = get_champs_table(league = "NBA")[["Teams", "Win", "League"]]
-# champ_df = champ_df.rename(columns = {"Teams": "Team", "Win": "Wins"})
-# champ_df["Team"] = champ_df["Team"].str.replace(r"\[.*\]", "")
-# champ_df["Wins"] = champ_df["Wins"].replace("—", 0)
+        elif league == "NHL":
+                champ_df = get_champs_table(league = league)[["Team", "Wins", "League"]]
+                champ_df["Team"] = champ_df["Team"].str.replace(r"\[.\]", "")
 
-# NFL DONE
-# champ_df = get_champs_table(league = "NFL")[["Team", "Wins", "League"]]
-# champ_df = champ_df[: -1] # remove last header row
-# champ_df["Team"] = champ_df["Team"].str.replace(r"\[.*\]", "") # get rid of bracketed numbers
-# champ_df["Team"] = champ_df["Team"].str.rstrip(r"(N|n|A|a)")   # get rid of text artifacts
-# champ_df = champ_df.replace({"Boston/New England Patriots": "New England Patriots",
-#                              "Oakland/Los Angeles Raiders": "Oakland Raiders",
-#                              "Baltimore/Indianapolis Colts": "Indianapolis Colts",
-#                              "St. Louis/Los Angeles Rams": "Los Angeles Rams",
-#                              "San Diego/Los Angeles Chargers": "Los Angeles Chargers",
-#                              "Houston/Tennessee Oilers/Titans": "Tennessee Titans",
-#                              "St. Louis/Phoenix/Arizona Cardinals": "Arizona Cardinals"})
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
 
-print(champ_df)
+        elif league == "MLB":
+                champ_df = get_champs_table(league = league)[["Team", "Wins", "League"]]
+                champ_df = champ_df.replace({"New York/San Francisco GiantsNY until 1957, SF 1958-present": "San Francisco Giants",
+                                             "Brooklyn/Los Angeles DodgersBKN until 1957, LA 1958-present": "Los Angeles Dodgers",
+                                             "Philadelphia/Kansas City/Oakland AthleticsPHI until 1954, KC 1955-1967, OAK 1968-present": "Oakland Athletics",
+                                             "Boston/Milwaukee/Atlanta BravesBOS until 1952, MIL 1953-1965, ATL 1966-present": "Atlanta Braves",
+                                             "St. Louis Browns/Baltimore OriolesSTL until 1953, BAL 1954-present": "Baltimore Orioles",
+                                             "Washington Senators/Minnesota TwinsWSH until 1960, MIN 1961-present": "Minnesota Twins",
+                                             "Washington Senators/Texas Rangers WSH until 1971, TEX 1972-present": "Texas Rangers",
+                                             "Montreal Expos/Washington Nationals MTL until 2004, WSH 2005-present": "Washington Nationals",
+                                             "Seattle Pilots/Milwaukee Brewers SEA 1969, MIL 1970-present": "Milwaukee Brewers"})
+
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
+
+        elif league == "NBA":
+                champ_df = get_champs_table(league = league)[["Teams", "Win", "League"]]
+                champ_df = champ_df.rename(columns = {"Teams": "Team", "Win": "Wins"})
+                champ_df["Team"] = champ_df["Team"].str.replace(r"\[.*\]", "")
+                champ_df["Wins"] = champ_df["Wins"].replace("—", 0)
+
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
+
+        elif league == "NFL":
+                champ_df = get_champs_table(league = league)[["Team", "Wins", "League"]]
+                champ_df = champ_df[: -1] # remove last header row
+                champ_df["Team"] = champ_df["Team"].str.replace(r"\[.*\]", "") # get rid of bracketed numbers
+                champ_df["Team"] = champ_df["Team"].str.rstrip(r"(N|n|A|a)")   # get rid of text artifacts
+                champ_df = champ_df.replace({"Boston/New England Patriots": "New England Patriots",
+                                             "Oakland/Los Angeles Raiders": "Oakland Raiders",
+                                             "Baltimore/Indianapolis Colts": "Indianapolis Colts",
+                                             "St. Louis/Los Angeles Rams": "Los Angeles Rams",
+                                             "San Diego/Los Angeles Chargers": "Los Angeles Chargers",
+                                             "Houston/Tennessee Oilers/Titans": "Tennessee Titans",
+                                             "St. Louis/Phoenix/Arizona Cardinals": "Arizona Cardinals"})
+
+                champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
+
 
 #champ_df.to_csv(f"data/{league}_Champions.csv", index = False)
